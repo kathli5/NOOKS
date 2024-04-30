64f09d4c46283c54248b2d1cb78b1b9489158429
Anna Zhou <az108@wellesley.edu>
2024-04-30 08:28:46 -0400

    part of the like feature

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ var g;
 function nextIfOk(resp) {
     g = resp;
     console.log('response received');
-    if(resp.status === 200) {
+    if (resp.status === 200) {
         return resp.json();
     } else {
         throw new Error('Something went wrong on server!');
@@ -18,16 +18,19 @@ function loginAjax() {
     console.log('form', form);
     let form_data = new FormData(form);
     console.log('data', form_data);
-    const req = new Request('/set-uid-ajax/', {method: 'POST',
-                                               body: form_data});
+    const req = new Request('/set-uid-ajax/', {
+        method: 'POST',
+        body: form_data
+    });
     fetch(req)
         .then(nextIfOk)
-        .then((resp) => { console.debug(resp);
-                          // update page for logged-in user
-                          $("#login-uid").text(uid);
-                          $("#logged-in").show();
-                          $("#not-logged-in").hide();
-                        })
+        .then((resp) => {
+            console.debug(resp);
+            // update page for logged-in user
+            $("#login-uid").text(uid);
+            $("#logged-in").show();
+            $("#not-logged-in").hide();
+        })
         .catch((error) => { console.error(error); });
 }
 
@@ -35,3 +38,28 @@ $("#login-ajax").click(loginAjax);
 
 console.log('main.js loaded');
 
+
+$(document).ready(() => {
+    $('.likebutton').click((e) => {
+        let nid = $(e.currentTarget).attr("class").split(/\s+/)[1];
+        console.log(nid);
+        likeNook(nid);
+    })
+
+    function likeNook(nid) {
+        $.post("/like/" + nid, { nid: nid }).then((res) => {
+            if (res.change) {
+                $(`.${nid}`).html(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+          </svg>`)
+          console.log($(`.${nid}`).prev())
+          $(`.${nid}`).prev().text(res.likes);
+            } else {
+                $(`.${nid}`).html(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+          </svg>`)
+          $(`.${nid}`).prev().text(res.likes)
+            }
+        });
+    }
+})
